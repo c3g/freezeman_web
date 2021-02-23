40ffd3d5aba2ae2f3cd2df2fdc85c1b610829714
Rom Grk <romgrk.cc@gmail.com>
2021-02-23 14:10:42 -0500

    tests: add container add test

--- cypress/integration/all.spec.js
+++ cypress/integration/all.spec.js
@@ -10,49 +10,71 @@
 //  - Sample_submission_v0.9.1_AB.xlsx
 //  - Sample_update_v0.5.xlsx
 
+// Helpers
+const getCredentials = () =>
+  cy.fixture('credentials')
+
+const login = (credentials) => {
+  cy.get('[autocomplete=username]').type(credentials.user)
+  cy.get('input[type=password]').type(credentials.password)
+  cy.get('form').submit()
+}
+
+const navigateTo = (section, button) => {
+  cy.get('.ant-layout-sider li').contains(section).click()
+  if (button)
+    cy.get('button').contains(button).click()
+}
+
+const submit = () =>
+  cy.get('button').contains('Submit').click()
+
+
+// Tests
 context('All tests', () => {
 
-  // Helpers
-  const getCredentials = () =>
-    cy.fixture('credentials')
-
-  const login = (credentials) => {
-    cy.get('[autocomplete=username]').type(credentials.user)
-    cy.get('input[type=password]').type(credentials.password)
-    cy.get('form').submit()
-  }
-
-  // Tests
   beforeEach(() => {
     cy.visit('http://localhost:9000/sign-in')
+    getCredentials().then(login)
   })
 
   it('logs in', () => {
-    getCredentials().then(login)
     cy.url().should('contain', '/dashboard')
-    // cy.contains('Welcome, Admin')
   })
 
-  it('creates containers', () => {
-    getCredentials().then(login)
-    cy.get('li').contains('Containers').click()
-    cy.get('button').contains('Add Containers').click()
-    cy.get('input[type=file]').attachFile('Container_creation_v0.4.xlsx')
-    cy.get('button').contains('Submit').click()
-    cy.get('.ant-alert-success').should('contain', 'Template submitted')
-    cy.get('button').contains('Go Back').click()
-    cy.get('body').should('contain', '1-10 of 14 items')
+  context('Containers', () => {
+    it.only('creates single container', () => {
+      navigateTo('Container', 'Add')
+      const name = 'test-container-add'
+      const comment = 'This is a comment.'
+      cy.get('#name').type(name)
+      cy.get('#kind').click()
+      cy.get('.ant-select-dropdown .ant-select-item-option').first().click()
+      cy.get('#barcode').type(name)
+      cy.get('#comment').type(comment)
+      submit()
+      cy.get('body').should('contain', `Container ${name}`) // Details title
+    })
+
+    it('creates multiple containers (template import)', () => {
+      navigateTo('Container', 'Add Containers')
+      cy.get('input[type=file]').attachFile('Container_creation_v0.4.xlsx')
+      submit()
+      cy.get('.ant-alert-success').should('contain', 'Template submitted')
+      cy.get('button').contains('Go Back').click()
+      cy.get('body').should('contain', '1-10 of 14 items')
+    })
   })
 
-  it('creates samples', () => {
-    getCredentials().then(login)
-    cy.get('li').contains('Samples & Extractions').click()
-    cy.get('button').contains('Add Samples').click()
-    cy.get('input[type=file]').attachFile('Sample_submission_v0.9.1_AB.xlsx')
-    cy.get('button').contains('Submit').click()
-    cy.get('.ant-alert-success').should('contain', 'Template submitted')
-    cy.get('button').contains('Go Back').click()
-    cy.get('body').should('contain', '1-8 of 8 items')
+  context('Samples', () => {
+    it('creates samples (template import)', () => {
+      navigateTo('Samples & Extractions', 'Add Samples')
+      cy.get('input[type=file]').attachFile('Sample_submission_v0.9.1_AB.xlsx')
+      submit()
+      cy.get('.ant-alert-success').should('contain', 'Template submitted')
+      cy.get('button').contains('Go Back').click()
+      cy.get('body').should('contain', '1-8 of 8 items')
+    })
   })
 
 
